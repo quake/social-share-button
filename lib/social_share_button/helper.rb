21ed8edab86699c656f88899ef43b2a4f0a8085c
quake wang <quake.wang@gmail.com>
2013-06-27 14:22:01 +0800

    add sites to opts

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -7,7 +7,7 @@ module SocialShareButton
       html = []
       html << "<div class='social-share-button' data-title='#{title}' data-img='#{opts[:image]}'>"
       
-      SocialShareButton.config.allow_sites.each do |name|
+      (opts[:sites] || SocialShareButton.config.allow_sites).each do |name|
         link_title = t "social_share_button.share_to", :name => t("social_share_button.#{name.downcase}")
         html << link_to("","#", :rel => "nofollow #{rel}", 
                         "data-site" => name, 
